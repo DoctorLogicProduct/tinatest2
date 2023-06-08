225907fd58c651afd1302bdaed32b0cd8d93b9ee
Robert Dennis <rdennis@doctorlogic.com>
2023-06-08 17:53:53 -0500

    Improved Grid block

--- components/blocks/Grid/Grid.tsx
+++ components/blocks/Grid/Grid.tsx
@@ -1,11 +1,11 @@
 import * as React from "react";
-import { HomeBlocksGrid, HomeBlocksGridItems } from '../../../.tina/__generated__/types';
+import { HomeBlocksGrid, HomeBlocksGridGroups, HomeBlocksGridGroupsItems } from '../../../.tina/__generated__/types';
 import { BlockComponent } from '../_shared';
 
 import styles from "./Grid.module.scss";
 
 type GridItemParams = {
-  data: HomeBlocksGridItems
+  data: HomeBlocksGridGroupsItems
   tinaField: string
 }
 
@@ -14,16 +14,15 @@ const getLayoutClass = (layout: string) => styles[`grid--${layout}`];
 const GridItem = ({ data, tinaField }: GridItemParams) => {
   return (
     <div
-      data-tinafield={`${tinaField}`}
-      className={styles.grid}
-    >
+      data-tinafield={`${tinaField}`}>
       {(
-        (data.image || []).length > 0 ?
-          <div 
-          className={styles.image}
-          data-tinafield={`${tinaField}.image`}
-          >
-            <img src={data.image} alt={data.image_alt} />
+        (data.image || '').length > 0 ?
+          <div
+            className={styles.image}
+            data-tinafield={`${tinaField}.image`}>
+            <img
+              src={data.image}
+              alt={data.image_alt} />
           </div> :
           null
       )}
@@ -31,16 +30,14 @@ const GridItem = ({ data, tinaField }: GridItemParams) => {
       {data.title && (
         <h3
           data-tinafield={`${tinaField}.title`}
-          className={styles.headline}
-        >
+          className={styles.headline}>
           {data.title}
         </h3>
       )}
       {data.text && (
         <p
           data-tinafield={`${tinaField}.text`}
-          className={styles.text}
-        >
+          className={styles.text}>
           {data.text}
         </p>
       )}
@@ -48,35 +45,77 @@ const GridItem = ({ data, tinaField }: GridItemParams) => {
         <a
           data-tinafield={`${tinaField}.btn_link`}
           className={styles.button}
-          href={data.btn_link}
-        >
+          href={data.btn_link}>
           <span
-          data-tinafield={`${tinaField}.btn_label`}
-          >
+            data-tinafield={`${tinaField}.btn_label`}>
             {data.btn_label}
           </span>
-          
+
         </a>
       )}
     </div>
   );
 };
 
+type GridGroupParams = {
+  inputName: string
+  inputId: string
+  checked: boolean
+  data: HomeBlocksGridGroups
+  tinaField: string
+}
+
+const GridGroup = ({ inputName, inputId, checked, data, tinaField }: GridGroupParams) => {
+  // todo: add classes and probably other markup
+  return data.items?.length > 0 ?
+    (<>
+      <input
+        type="radio"
+        id={inputId}
+        name={inputName}
+        defaultChecked={checked} />
+      <label
+        htmlFor={inputId}
+        data-tinafield={tinaField}>
+        <span
+          data-tinafield={`${tinaField}.label`}>
+          {data.label}
+        </span>
+      </label>
+      <div
+        className={styles.group}>
+        {
+          data.items.map((block, i) => {
+            const key = `${tinaField}.items.${i}`;
+            return (
+              <GridItem
+                key={key}
+                tinaField={key}
+                data={block} />
+            );
+          })
+        }
+      </div>
+    </>)
+    : null;
+};
+
 export const Grid: BlockComponent<HomeBlocksGrid> = ({ data, parentField }) => {
+  const inputName = `grid-toggle-${Math.floor(Math.random() * 100) + 1}`;
+
   return (
     <div
-      className={`${styles.grid} ${getLayoutClass(data.layout)}`}
-    >
-      {data.items &&
-        data.items.map(function (block, i) {
-          return (
-            <GridItem
-              tinaField={`${parentField}.items.${i}`}
-              key={i}
-              data={block}
-            />
-          );
-        })}
+      className={`${styles.grid} ${getLayoutClass(data.layout)}`}>
+      {data.groups &&
+        data.groups.map((block, i) => (
+          <GridGroup
+            key={i}
+            tinaField={`${parentField}.groups.${i}`}
+            inputId={`${inputName}-${i}`}
+            checked={i === 0}
+            inputName={inputName}
+            data={block} />
+        ))}
     </div>
   );
 };
--- components/blocks/Grid/_schema.ts
+++ components/blocks/Grid/_schema.ts
@@ -1,17 +1,26 @@
 import { TinaTemplate } from 'tinacms';
 
-const defaultGridItem = {
-  title: "Here's Another Feature",
-  text: "This is where you might talk about the feature, if this wasn't just filler text.",
-  image: "",
-  image_alt: "This is the image alt text.",
-  // there is no `icon` field on the
-  // icon: {
-  //     color: "",
-  //     style: "float",
-  //     name: "",
-  // },
-};
+function makeDefaultGridItem({ title = '', text = '' } = {}) {
+  return {
+    title: title || "Here's Another Item",
+    text: text || "This is where you might talk about the item, if this wasn't just filler text.",
+    image: "",
+    image_alt: "This is the image alt text.",
+    // there is no `icon` field on the
+    // icon: {
+    //     color: "",
+    //     style: "float",
+    //     name: "",
+    // },
+  };
+}
+
+function makeDefaultGroup(params?: { groupLabel?: string } & Parameters<typeof makeDefaultGridItem>[0]) {
+  return {
+    label: params?.groupLabel || "New Group",
+    items: [makeDefaultGridItem(params)],
+  };
+}
 
 export const gridBlock: TinaTemplate = {
   name: "grid",
@@ -19,53 +28,80 @@ export const gridBlock: TinaTemplate = {
   ui: {
     previewSrc: "/blocks/features.png",
     defaultItem: {
-      items: [defaultGridItem, defaultGridItem, defaultGridItem],
+      groups: [
+        makeDefaultGroup({
+          groupLabel: 'Group 1',
+          title: 'Item 1.1',
+        }),
+        makeDefaultGroup({
+          groupLabel: 'Group 2',
+          title: 'Item 2.1',
+        })
+      ],
     },
   },
   fields: [
     {
       type: "object",
-      label: "Grid Items",
-      name: "items",
+      name: "groups",
+      label: "Groups",
       list: true,
       ui: {
         defaultItem: {
-          ...defaultGridItem,
+          ...makeDefaultGroup(),
         },
       },
       fields: [
         {
           type: "string",
-          label: "Title",
-          name: "title",
+          name: "label",
+          label: "Label",
         },
         {
-          type: "string",
-          label: "Text",
-          name: "text",
+          type: "object",
+          name: "items",
+          label: "Group Items",
+          list: true,
           ui: {
-            component: "textarea",
+            defaultItem: {
+              ...makeDefaultGridItem(),
+            },
           },
-        },
-        {
-          type: 'image',
-          label: 'Image',
-          name: 'image',
-        },
-        {
-          type: 'string',
-          label: 'Image Alt Tag',
-          name: 'image_alt',
-        },
-        {
-          type: 'string',
-          label: 'Button Link',
-          name: 'btn_link',
-        },
-        {
-          type: 'string',
-          label: 'Button Label',
-          name: 'btn_label',
+          fields: [
+            {
+              type: "string",
+              label: "Title",
+              name: "title",
+            },
+            {
+              type: "string",
+              label: "Text",
+              name: "text",
+              ui: {
+                component: "textarea",
+              },
+            },
+            {
+              type: 'image',
+              label: 'Image',
+              name: 'image',
+            },
+            {
+              type: 'string',
+              label: 'Image Alt Tag',
+              name: 'image_alt',
+            },
+            {
+              type: 'string',
+              label: 'Button Link',
+              name: 'btn_link',
+            },
+            {
+              type: 'string',
+              label: 'Button Label',
+              name: 'btn_label',
+            },
+          ],
         },
       ],
     },
