56bc2e19362dd43902e45758ea960c43e6e28d6e
matthewsparks75 <msparks@doctorlogic.com>
2023-06-13 12:36:45 -0500

    tina updates

--- components/blocks/Grid/Grid.tsx
+++ components/blocks/Grid/Grid.tsx
@@ -1,19 +1,23 @@
-import * as React from "react";
+import React, { Dispatch, FC, MouseEventHandler, SetStateAction, useEffect, useLayoutEffect, useState } from "react";
 import { HomeBlocksGrid, HomeBlocksGridGroups, HomeBlocksGridGroupsItems } from '../../../.tina/__generated__/types';
 import { BlockComponent } from '../_shared';
 
 import styles from "./Grid.module.scss";
+import { classNames } from "tinacms";
+
 
 type GridItemParams = {
+  setSelectedItem: Dispatch<SetStateAction<HomeBlocksGridGroupsItems>>
   data: HomeBlocksGridGroupsItems
   tinaField: string
 }
 
 const getLayoutClass = (layout: string) => styles[`grid--${layout}`];
 
-const GridItem = ({ data, tinaField }: GridItemParams) => {
+const GridItem = ({ data, tinaField, setSelectedItem }: GridItemParams) => {
   return (
     <div
+      onClick={() => setSelectedItem(data)}
       data-tinafield={`${tinaField}`}>
       {(
         (data.image || '').length > 0 ?
@@ -23,41 +27,24 @@ const GridItem = ({ data, tinaField }: GridItemParams) => {
             <img
               src={data.image}
               alt={data.image_alt} />
+            {data.title && (
+              <h3
+                className={styles.image__headline}
+                data-tinafield={`${tinaField}.title`}>
+                {data.title}
+              </h3>
+            )}
           </div> :
           null
-      )}
-
-      {data.title && (
-        <h3
-          data-tinafield={`${tinaField}.title`}
-          className={styles.headline}>
-          {data.title}
-        </h3>
-      )}
-      {data.text && (
-        <p
-          data-tinafield={`${tinaField}.text`}
-          className={styles.text}>
-          {data.text}
-        </p>
-      )}
-      {data.btn_label && (
-        <a
-          data-tinafield={`${tinaField}.btn_link`}
-          className={styles.button}
-          href={data.btn_link}>
-          <span
-            data-tinafield={`${tinaField}.btn_label`}>
-            {data.btn_label}
-          </span>
-
-        </a>
       )}
     </div>
   );
 };
 
+
+
 type GridGroupParams = {
+  setSelectedItem: Dispatch<SetStateAction<HomeBlocksGridGroupsItems>>
   inputName: string
   inputId: string
   checked: boolean
@@ -65,7 +52,7 @@ type GridGroupParams = {
   tinaField: string
 }
 
-const GridGroup = ({ inputName, inputId, checked, data, tinaField }: GridGroupParams) => {
+const GridGroup = ({ setSelectedItem, inputName, inputId, checked, data, tinaField }: GridGroupParams) => {
   // todo: add classes and probably other markup
   return data.items?.length > 0 ?
     (<>
@@ -89,6 +76,7 @@ const GridGroup = ({ inputName, inputId, checked, data, tinaField }: GridGroupPa
             const key = `${tinaField}.items.${i}`;
             return (
               <GridItem
+                setSelectedItem={setSelectedItem}
                 key={key}
                 tinaField={key}
                 data={block} />
@@ -100,7 +88,81 @@ const GridGroup = ({ inputName, inputId, checked, data, tinaField }: GridGroupPa
     : null;
 };
 
+type GridModalParams = {
+  item: HomeBlocksGridGroupsItems
+  setSelectedItem: Dispatch<SetStateAction<HomeBlocksGridGroupsItems>>
+}
+
+const GridModal: FC<GridModalParams> = ({ item, setSelectedItem }) => {
+  const [className, setClassName] = useState(classNames(styles.modal, styles.modal_close));
+
+  useEffect(() => {
+    requestAnimationFrame(() => {
+      setTimeout(() => {
+        setClassName(styles.modal);
+      }, 1000);
+    });
+
+  }, [item]);
+
+  if (!item) {
+    return null;
+  }
+
+  const handleClick: MouseEventHandler<HTMLButtonElement> = (ev) => {
+    if (ev.target instanceof HTMLElement) {
+      const parent = ev.target.parentElement
+      parent.addEventListener('transitionend', () => setSelectedItem(null))
+      parent.classList.add(styles.modal_close)
+    }
+  }
+
+  return (
+    <div
+      className={className}>
+      <button
+        onClick={handleClick}>
+        X
+      </button>
+      {(
+        (item.image || '').length > 0 ?
+          <div
+            className={styles.image__modal}>
+            <img
+              src={item.image}
+              alt={item.image_alt} />
+          </div> :
+          null
+      )}
+
+      {item.title && (
+        <h3
+          className={styles.headline}>
+          {item.title}
+        </h3>
+      )}
+      {item.text && (
+        <p
+          className={styles.text}>
+          {item.text}
+        </p>
+      )}
+      {item.btn_label && (
+        <a
+          className={styles.button}
+          href={item.btn_link}>
+          <span>
+            {item.btn_label}
+          </span>
+
+        </a>
+      )}
+    </div>
+  );
+};
+
 export const Grid: BlockComponent<HomeBlocksGrid> = ({ data, parentField }) => {
+  const [selectedItem, setSelectedItem] = useState<HomeBlocksGridGroupsItems>();
   const inputName = `grid-toggle-${Math.floor(Math.random() * 100) + 1}`;
 
   return (
@@ -109,6 +171,7 @@ export const Grid: BlockComponent<HomeBlocksGrid> = ({ data, parentField }) => {
       {data.groups &&
         data.groups.map((block, i) => (
           <GridGroup
+            setSelectedItem={setSelectedItem}
             key={i}
             tinaField={`${parentField}.groups.${i}`}
             inputId={`${inputName}-${i}`}
@@ -116,6 +179,10 @@ export const Grid: BlockComponent<HomeBlocksGrid> = ({ data, parentField }) => {
             inputName={inputName}
             data={block} />
         ))}
+      <GridModal
+        item={selectedItem}
+        setSelectedItem={setSelectedItem}
+      />
     </div>
   );
 };
