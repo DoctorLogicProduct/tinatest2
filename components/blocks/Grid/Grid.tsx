1a55bc801d45cd2f071cab5b7af9371e2b8e0ab5
Robert Dennis <rdennis@doctorlogic.com>
2023-06-13 23:15:48 -0500

    Grid block modal

--- components/blocks/Grid/Grid.tsx
+++ components/blocks/Grid/Grid.tsx
@@ -1,9 +1,10 @@
-import React, { Dispatch, FC, MouseEventHandler, SetStateAction, useEffect, useLayoutEffect, useState } from "react";
+import React, { Dispatch, FC, MouseEventHandler, SetStateAction, useId, useState } from "react";
+import { classNames } from "tinacms";
+
 import { HomeBlocksGrid, HomeBlocksGridGroups, HomeBlocksGridGroupsItems } from '../../../.tina/__generated__/types';
 import { BlockComponent } from '../_shared';
 
 import styles from "./Grid.module.scss";
-import { classNames } from "tinacms";
 
 
 type GridItemParams = {
@@ -46,23 +47,24 @@ const GridItem = ({ data, tinaField, setSelectedItem }: GridItemParams) => {
 type GridGroupParams = {
   setSelectedItem: Dispatch<SetStateAction<HomeBlocksGridGroupsItems>>
   inputName: string
-  inputId: string
   checked: boolean
   data: HomeBlocksGridGroups
   tinaField: string
 }
 
-const GridGroup = ({ setSelectedItem, inputName, inputId, checked, data, tinaField }: GridGroupParams) => {
+const GridGroup = ({ setSelectedItem, inputName, checked, data, tinaField }: GridGroupParams) => {
   // todo: add classes and probably other markup
+  const id = useId();
+
   return data.items?.length > 0 ?
     (<>
       <input
         type="radio"
-        id={inputId}
+        id={id}
         name={inputName}
         defaultChecked={checked} />
       <label
-        htmlFor={inputId}
+        htmlFor={id}
         data-tinafield={tinaField}>
         <span
           data-tinafield={`${tinaField}.label`}>
@@ -90,80 +92,64 @@ const GridGroup = ({ setSelectedItem, inputName, inputId, checked, data, tinaFie
 
 type GridModalParams = {
   item: HomeBlocksGridGroupsItems
-  setSelectedItem: Dispatch<SetStateAction<HomeBlocksGridGroupsItems>>
+  isOpen: boolean
+  onCloseClick: MouseEventHandler<HTMLButtonElement>
 }
 
-const GridModal: FC<GridModalParams> = ({ item, setSelectedItem }) => {
-  const [className, setClassName] = useState(classNames(styles.modal, styles.modal_close));
-
-  useEffect(() => {
-    requestAnimationFrame(() => {
-      setTimeout(() => {
-        setClassName(styles.modal);
-      }, 1000);
-    });
-
-  }, [item]);
-
-  if (!item) {
-    return null;
-  }
-
-  const handleClick: MouseEventHandler<HTMLButtonElement> = (ev) => {
-    if (ev.target instanceof HTMLElement) {
-      const parent = ev.target.parentElement
-      parent.addEventListener('transitionend', () => setSelectedItem(null))
-      parent.classList.add(styles.modal_close)
-    }
-  }
-
+const GridModal: FC<GridModalParams> = ({ item = {}, isOpen, onCloseClick }) => {
   return (
     <div
-      className={className}>
-      <button
-        onClick={handleClick}>
-        X
-      </button>
-      {(
-        (item.image || '').length > 0 ?
-          <div
-            className={styles.image__modal}>
-            <img
-              src={item.image}
-              alt={item.image_alt} />
-          </div> :
-          null
-      )}
+      className={classNames(styles.modal, isOpen ? styles.modal_open : styles.modal_close)}
+      aria-hidden={!isOpen}>
+      <div
+        className={styles.modal_content}>
+        <button
+          onClick={onCloseClick}>
+          X
+        </button>
+        {(
+          (item.image || '').length > 0 ?
+            <div
+              className={styles.image__modal}>
+              <img
+                src={item.image}
+                alt={item.image_alt} />
+            </div> :
+            null
+        )}
 
-      {item.title && (
-        <h3
-          className={styles.headline}>
-          {item.title}
-        </h3>
-      )}
-      {item.text && (
-        <p
-          className={styles.text}>
-          {item.text}
-        </p>
-      )}
-      {item.btn_label && (
-        <a
-          className={styles.button}
-          href={item.btn_link}>
-          <span>
-            {item.btn_label}
-          </span>
+        {item.title && (
+          <h3
+            className={styles.headline}>
+            {item.title}
+          </h3>
+        )}
+        {item.text && (
+          <p
+            className={styles.text}>
+            {item.text}
+          </p>
+        )}
+        {item.btn_label && (
+          <a
+            className={styles.button}
+            href={item.btn_link}>
+            <span>
+              {item.btn_label}
+            </span>
 
-        </a>
-      )}
+          </a>
+        )}
+      </div>
     </div>
   );
 };
 
 export const Grid: BlockComponent<HomeBlocksGrid> = ({ data, parentField }) => {
   const [selectedItem, setSelectedItem] = useState<HomeBlocksGridGroupsItems>();
-  const inputName = `grid-toggle-${Math.floor(Math.random() * 100) + 1}`;
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const gridToggleId = useId();
+  const inputName = `grid-toggle-${gridToggleId}`;
 
   return (
     <div
@@ -171,17 +157,20 @@ export const Grid: BlockComponent<HomeBlocksGrid> = ({ data, parentField }) => {
       {data.groups &&
         data.groups.map((block, i) => (
           <GridGroup
-            setSelectedItem={setSelectedItem}
+            setSelectedItem={item => {
+              setSelectedItem(item);
+              setIsModalOpen(true);
+            }}
             key={i}
             tinaField={`${parentField}.groups.${i}`}
-            inputId={`${inputName}-${i}`}
             checked={i === 0}
             inputName={inputName}
             data={block} />
         ))}
       <GridModal
         item={selectedItem}
-        setSelectedItem={setSelectedItem}
+        isOpen={isModalOpen}
+        onCloseClick={() => setIsModalOpen(false)}
       />
     </div>
   );
