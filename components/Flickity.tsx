b8f316d5ad69f1b3c80ba219880fee06e887a16f
Robert Dennis <rdennis@doctorlogic.com>
2023-08-01 10:19:27 -0500

    Better Flickity component

--- components/Flickity.tsx
+++ components/Flickity.tsx
@@ -1,4 +1,4 @@
-import React, { ReactNode, useId, useRef } from 'react';
+import React, { ReactNode, useId } from 'react';
 import type FlickityJs from 'flickity';
 
 export type FlickityProps = {
@@ -10,27 +10,23 @@ export type FlickityProps = {
 export function Flickity({ children, flickityRef: flickityRefProp, options }: FlickityProps) {
   const id = useId();
 
-  const elRef = useRef<HTMLElement>();
-  const flickityRef = useRef<FlickityJs>();
-
-  if (!flickityRef.current && elRef.current) {
-    setup();
-  }
-
   return (
     <div
       id={id}
-      ref={el => elRef.current = el}
+      ref={el => {
+        if (el) {
+          setup(el);
+        }
+      }}
     >
       {children}
     </div>
   );
 
-  async function setup() {
+  async function setup(el: HTMLElement) {
     const FlickityJs = (await import('flickity')).default;
-    const flickity = new FlickityJs(elRef.current, options);
+    const flickity = new FlickityJs(el, options);
 
-    flickityRef.current = flickity;
     flickityRefProp(flickity);
   }
 }
